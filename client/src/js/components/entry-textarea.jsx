ae87ff1538b793936ba56d5e089666cf2c95692e
Mark Reid <contact@markreid.com.au>
2016-09-24 13:09:47 +1000

    Update entry-textarea.jsx for new React compatibility

--- client/src/js/components/entry-textarea.jsx
+++ client/src/js/components/entry-textarea.jsx
@@ -3,81 +3,90 @@
  * Textarea for entries
  */
 
-import React from 'react';
-import {Component} from 'react';
-import {connect} from 'react-redux';
+import React, { Component } from 'react';
+import autobind from 'autobind-decorator';
 
-export default class EntryTextarea extends Component{
+@autobind
+export default class EntryTextarea extends Component {
 
-    constructor(){
-        super();
-        this.state = {
-            style: {}
-        };
+  static propTypes() {
+    return {
+      value: React.PropTypes.string,
+      onChange: React.PropTypes.function,
+      onBlur: React.PropTypes.function,
+    };
+  }
+
+  constructor() {
+    super();
+    this.state = {
+      style: {},
+    };
+  }
+
+  componentDidMount() {
+    // set initial height
+    this.setHeight(this.textarea);
+  }
+
+  onChange(evt) {
+    // update the textarea height
+    this.setHeight(evt.currentTarget);
+
+    const { currentTarget } = evt;
+
+    let caretPos = evt.currentTarget.selectionStart;
+    const value = evt.currentTarget.value;
+
+    // first character, prefix with a bullet.
+    if (value.length === 1 && value !== '•') {
+      currentTarget.value = `• ${value}`;
+      caretPos = 3;
     }
 
-    render(){
-        return <textarea style={this.state.style} onChange={this.onChange.bind(this)} onBlur={this.onBlur.bind(this)} value={this.props.value}></textarea>
+    // add a bullet after a newline
+    if (value.charAt(caretPos - 2) === '\n' && value.charAt(caretPos - 1) !== '•') {
+      currentTarget.value = `${value.substr(0, caretPos - 1)}• ${value.substr(caretPos - 1)}`;
+      caretPos += 2;
     }
 
-    onChange(evt){
-        // update the textarea height
-        this.setHeight(evt.currentTarget);
+    currentTarget.selectionStart = caretPos;
+    currentTarget.selectionEnd = caretPos;
 
-        var caretPos = evt.currentTarget.selectionStart;
-        var value = evt.currentTarget.value;
+    // pass up to any onChange handler put on the component
+    if (this.props.onChange) this.props.onChange(evt);
+  }
 
-        // first character, prefix with a bullet.
-        if(value.length === 1 && value !== '•'){
-            evt.currentTarget.value = '• ' + value;
-            caretPos = 3;
-        }
+  onBlur(evt) {
+    // pass to the onBlur handler of the component, if it exists
+    if (this.props.onBlur) this.props.onBlur(evt);
+  }
 
-        // add a bullet after a newline
-        if(value.charAt(caretPos-2) === '\n' && value.charAt(caretPos-1) !== '•'){
-            evt.currentTarget.value = value.substr(0, caretPos-1) + '• ' + value.substr(caretPos-1);
-            caretPos += 2;
-        }
+  // reset the height of the textarea by setting it to auto, calculating
+  // the height of the scroll area, and then resizing.
+  setHeight(textarea) {
+    this.setState({
+      style: {
+        height: 'auto',
+      },
+    });
+    requestAnimationFrame(() => {
+      this.setState({
+        style: {
+          height: `${textarea.scrollHeight}px`,
+        },
+      });
+    });
+  }
 
-        evt.currentTarget.selectionStart = caretPos;
-        evt.currentTarget.selectionEnd = caretPos;
-
-        // pass up to any onChange handler put on the component
-        if(this.props.onChange) this.props.onChange(evt);
-
-
-    }
-
-    onBlur(evt){
-        // pass to the onBlur handler of the component, if it exists
-        if(this.props.onBlur) this.props.onBlur(evt);
-    }
-
-
-    componentDidMount(){
-        // update the height on initial render
-        var textarea = React.findDOMNode(this);
-        this.setHeight(textarea);
-    }
-
-
-    // reset the height of the textarea by setting it to auto, calculating
-    // the height of the scroll area, and then resizing.
-    setHeight(textarea){
-        this.setState({
-            style: {
-                height: 'auto'
-            }
-        });
-        requestAnimationFrame(() => {
-            this.setState({
-                style: {
-                    height: textarea.scrollHeight + 'px'
-                }
-            });
-        });
-
-    }
+  render() {
+    return (<textarea
+      ref={(c) => { this.textarea = c; }}
+      style={this.state.style}
+      onChange={this.onChange}
+      onBlur={this.onBlur}
+      value={this.props.value || ''}
+    />);
+  }
 
 }
-

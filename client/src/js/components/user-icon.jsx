637b9067d46a76b9488f21122cb3165c95a7cc3e
Mark Reid <contact@markreid.com.au>
2017-04-10 17:05:56 +1000

    Remove user editing from UserIcon component

--- client/src/js/components/user-icon.jsx
+++ client/src/js/components/user-icon.jsx
@@ -4,47 +4,13 @@
  */
 
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import autobind from 'autobind-decorator';
 
-import { updateUser } from '../actions';
-import store from '../store';
-
-import UserProfile from './user-profile.jsx';
-
+@autobind
 class UserIconComponent extends Component {
 
-  constructor() {
-    super();
-    this.state = {
-      editing: false,
-    };
-
-    this.clickHandler = this.clickHandler.bind(this);
-    this.disableEditing = this.disableEditing.bind(this);
-    this.saveProfile = this.saveProfile.bind(this);
-  }
-
-  clickHandler(evt) {
-    if (!evt.shiftKey) {
-      if (this.props.onClick) this.props.onClick(this.props.id);
-    } else {
-      this.setState({
-        editing: true,
-      });
-    }
-  }
-
-  saveProfile(props) {
-    const { id } = this.props;
-    store.dispatch(updateUser(Object.assign(props, {
-      id,
-    }))).then(this.disableEditing);
-  }
-
-  disableEditing() {
-    this.setState({
-      editing: false,
-    });
+  clickHandler() {
+    if (this.props.onClick) this.props.onClick(this.props.id);
   }
 
   render() {
@@ -52,14 +18,6 @@ class UserIconComponent extends Component {
     const initials = names.length > 1 ?
       (names[0][0] + names[names.length - 1][0]) :
       names[0][0] + names[0][names[0].length - 1];
-
-    if (this.state.editing) {
-      return (<UserProfile
-        {...this.props}
-        onSave={this.saveProfile}
-        onCancel={this.disableEditing}
-      />);
-    }
 
     return <button className="user-icon" onClick={this.clickHandler}>{initials.toUpperCase()}</button>;
   }
